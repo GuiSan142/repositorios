50a0ef41815b290960e03105e376427c84e1eb8a
7728735 <7728735@U702607.univali.br>
2022-05-16 16:52:17 -0300

    CM - Revise!

--- src/archive/Archive.java
+++ src/archive/Archive.java
@@ -2,9 +2,16 @@ package archive;
 
 public class Archive {
     
-
+static int mathematics(int a, int b){
+    int c;
+    c = a+b;
+    return c;
+}
     public static void main(String[] args) {
-        
+        int a = 1,b = 5,c = 0;
+        int d;
+        d = mathematics(a,b);
+        System.out.println(d);
     }
     
 }
