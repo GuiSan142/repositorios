0627073e434f110cc2ac0715ce161d4fe69161e9
Bernar-duarte <60206917+Bernar-duarte@users.noreply.github.com>
2022-05-16 17:02:19 -0300

    CM - criei a funcao para diminuir

--- src/archive/Archive.java
+++ src/archive/Archive.java
@@ -7,10 +7,20 @@ static int mathematics(int a, int b){
     c = a+b;
     return c;
 }
+
+static int diminui(int a, int b){
+    int c;
+    c = a-b;
+    return c;
+}
+
+
+//tirei o c;
     public static void main(String[] args) {
         int a = 1,b = 5,c = 0;
         int d;
         d = mathematics(a,b);
+        d = diminui(a,b);
         System.out.println(d);
     }
     
